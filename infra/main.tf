6a26116a65801e088e1c90668b323df21feb6a32
omar-e <omar.hassanabdi20@gmail.com>
2026-02-12 10:40:26 +0000

    operational

--- infra/main.tf
+++ infra/main.tf
@@ -126,6 +126,14 @@ resource "aws_vpc_security_group_ingress_rule" "http" {
   ip_protocol       = "tcp"
   to_port           = 80
 }
+resource "aws_vpc_security_group_ingress_rule" "ssh" {
+  security_group_id = aws_security_group.ALB-2.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.ALB-2.id
