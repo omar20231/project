844450edbf2859eab8208209a32a97517629b5e5
omar-e <omar.hassanabdi20@gmail.com>
2026-02-24 16:20:30 +0000

    quick fix

--- ecs-assignment/src/serviceWorkerRegistration.ts
+++ ecs-assignment/src/serviceWorkerRegistration.ts
@@ -45,7 +45,7 @@ export function register(config?: Config) {
 
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
-        navigator.serviceWorker.ready.then(() => {
+        void navigator.serviceWorker.ready.then(() => {
           console.log(
             'This web app is being served cache-first by a service ' +
               'worker. To learn more, visit https://cra.link/PWA',
@@ -116,8 +116,8 @@ function checkValidServiceWorker(swUrl: string, config?: Config) {
         (contentType != null && contentType.indexOf('javascript') === -1)
       ) {
         // No service worker found. Probably a different app. Reload the page.
-        navigator.serviceWorker.ready.then((registration) => {
-          registration.unregister().then(() => {
+        void navigator.serviceWorker.ready.then((registration) => {
+          void registration.unregister().then(() => {
             window.location.reload();
           });
         });
@@ -137,7 +137,7 @@ export function unregister() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready
       .then((registration) => {
-        registration.unregister();
+       void  registration.unregister();
       })
       .catch((error) => {
         console.error(error.message);
